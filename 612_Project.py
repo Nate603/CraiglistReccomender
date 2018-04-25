fcfb565983ab0695d48dbd5dab9282c17b108974
Zack <zag2000@wildcats.unh.edu>
2018-04-25 17:10:51 -0400

    results

--- 612_Project.py
+++ 612_Project.py
@@ -57,71 +57,71 @@ trans=''
 location=''
 
 if results.miles:
-    location= "&search_distance=" + str(results.location)
+    location= "&search_distance=" + str(results.miles)
     baseUrl = baseUrl + location
     
 if results.postal:
-    miles= "&postal=" + str(results.miles)
+    miles= "&postal=" + str(results.postal)
     baseUrl = baseUrl + miles
     
 if results.min_price:
-    location= "&min_price=" + str(results.location)
+    location= "&min_price=" + str(results.min_price)
     baseUrl = baseUrl + location
     
 if results.max_price:
-    miles= "&max_price=" + str(results.miles)
+    miles= "&max_price=" + str(results.max_miles)
     baseUrl = baseUrl + miles
     
 if results.make:
-    location= "&auto_make_model=" + str(results.location)
+    location= "&auto_make_model=" + str(results.make)
     baseUrl = baseUrl + location
     
 if results.model:
-    miles= "&postal=" + str(results.miles)
+    miles= "&postal=" + str(results.model)
     baseUrl = baseUrl + miles
     
 if results.min_eng_disp:
-    location= "&min_engine_displacement_cc=" + str(results.location)
+    location= "&min_engine_displacement_cc=" + str(results.min_eng_disp)
     baseUrl = baseUrl + location
     
 if results.max_eng_disp:
-    miles= "&max_engine_displacement_cc=" + str(results.miles)
+    miles= "&max_engine_displacement_cc=" + str(results.max_eng_disp)
     baseUrl = baseUrl + miles
     
 if results.min_year:
-    location= "&min_auto_year=" + str(results.location)
+    location= "&min_auto_year=" + str(results.min_year)
     baseUrl = baseUrl + location
     
 if results.max_year:
-    location= "&max_auto_year=" + str(results.location)
+    location= "&max_auto_year=" + str(results.max_year)
     baseUrl = baseUrl + location
     
 if results.min_odo:
-    location= "&min_auto_miles=" + str(results.location)
+    location= "&min_auto_miles=" + str(results.min_odo)
     baseUrl = baseUrl + location
     
 if results.max_odo:
-    location= "&max_auto_miles=" + str(results.location)
+    location= "&max_auto_miles=" + str(results.max_odo)
     baseUrl = baseUrl + location
     
 if results.condition:
-    location= "&condition=" + str(results.location)
+    location= "&condition=" + str(results.condition)
     baseUrl = baseUrl + location
     
 if results.fuel:
-    location= "&auto_fuel_type=" + str(results.location)
+    location= "&auto_fuel_type=" + str(results.fuel)
     baseUrl = baseUrl + location
     
 if results.color:
-    location= "&auto_paint=" + str(results.location)
+    location= "&auto_paint=" + str(results.color)
     baseUrl = baseUrl + location
     
 if results.title:
-    location= "&auto_title_status=" + str(results.location)
+    location= "&auto_title_status=" + str(results.title)
     baseUrl = baseUrl + location
     
 if results.trans:
-    location= "&auto_transmission=" + str(results.location)
+    location= "&auto_transmission=" + str(results.trans)
     baseUrl = baseUrl + location
     
 if results.location:
